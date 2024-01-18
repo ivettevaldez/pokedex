78dbdc6b47974fc500d1b343173ca8a6f02acc25
Ivette Valdez <ivette.valdez.e@gmail.com>
2024-01-18 19:52:06 +0100

    Migrate Gradle build config from Groovy to Kotlin DSL

---
+++ Pokedex/app/build.gradle.kts
@@ -0,0 +1,94 @@
+plugins {
+    id("com.android.application")
+    id("androidx.navigation.safeargs")
+    id("com.google.devtools.ksp")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.ivettevaldez.pokedex"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.ivettevaldez.pokedex"
+        minSdk = 23
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+    buildFeatures {
+        viewBinding = true
+        buildConfig = true
+    }
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    testOptions {
+        unitTests {
+            isIncludeAndroidResources = true
+        }
+    }
+}
+
+dependencies {
+
+    val daggerVersion = "2.48.1"
+    val lifecycleVersion = "2.7.0"
+    val navigationVersion = "2.7.6"
+    val retrofitVersion = "2.9.0"
+    val roomVersion = "2.6.1"
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.swiperefreshlayout:swiperefreshlayout:1.1.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:$lifecycleVersion")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:$lifecycleVersion")
+    implementation("androidx.navigation:navigation-fragment-ktx:$navigationVersion")
+    implementation("androidx.navigation:navigation-ui-ktx:$navigationVersion")
+    implementation("com.google.android.material:material:1.11.0")
+
+    // Dagger (dependency injection)
+    implementation("com.google.dagger:dagger:$daggerVersion")
+    ksp("com.google.dagger:dagger-compiler:$daggerVersion")
+
+    // Retrofit (networking)
+    implementation("com.squareup.retrofit2:retrofit:$retrofitVersion")
+    implementation("com.squareup.retrofit2:converter-gson:$retrofitVersion")
+
+    // Room (persistence)
+    implementation("androidx.room:room-runtime:$roomVersion")
+    implementation("androidx.room:room-ktx:$roomVersion")
+    ksp("androidx.room:room-compiler:2.6.1")
+
+    // GlideToVectorYou (image loading)
+    implementation("com.github.2coffees1team:GlideToVectorYou:v2.0.0")
+
+    // LiveData-CombineTuple-KT (helper functions for LiveData)
+    implementation("com.github.Zhuinden:livedata-combinetuple-kt:2.0.0")
+
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+    testImplementation("androidx.test:core:1.5.0")
+    testImplementation("junit:junit:4.13.2")
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+    testImplementation("org.mockito:mockito-core:4.5.1")
+    testImplementation("org.mockito.kotlin:mockito-kotlin:4.1.0")
+    testImplementation("org.robolectric:robolectric:4.7.3")
+
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+}
